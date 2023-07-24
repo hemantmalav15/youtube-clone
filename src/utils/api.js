6bc79b2fa037bb820d27ea37b825052b3333adc1
Hemant Malav <96330614+hemantmalav15@users.noreply.github.com>
2023-07-24 14:41:07 +0530

    api integrated

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,8 +5,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
     params: { hl: "en", gl: "US" },
     headers: {
-        "X-RapidAPI-Key":
-            process.env.REACT_APP_YOUTUBE_API_KEY,
+        "X-RapidAPI-Key": 'd6018e46ccmshf099284433bdfc3p122042jsn4a94c67b00d3',
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
